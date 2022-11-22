f586da2736de14a1f836223725b099efd167fc54
Cristina Rodriguez <yosola@gmail.com>
2022-11-22 10:37:10 -0800

    Testing env names

--- CycleRoute/server/db/db-connection.js
+++ CycleRoute/server/db/db-connection.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 const db = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DB_URL
   });
 
   module.exports = db;
